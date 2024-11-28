81166d416d0b5b6b39a3d71cd1cf406b53b9b865
Lapocalypsie <jrasolosoa@gmail.com>
2024-11-28 13:27:36 +0100

    update document language to French and add Google Ads script

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -2,8 +2,14 @@ import { Head, Html, Main, NextScript } from 'next/document';
 
 export default function Document() {
   return (
-    <Html lang="en">
-      <Head />
+    <Html lang="fr">
+      <Head>
+        <script
+          async
+          src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-1459536949152630"
+          crossOrigin="anonymous"
+        ></script>
+      </Head>
       <body>
         <Main />
         <NextScript />
